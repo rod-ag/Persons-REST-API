e427f536acf9361ae00438a09ef4eb74020be449
Rodrigo Alconchel <rodalcon@hotmail.com>
2019-12-09 14:12:15 +0100

    javadoc

--- personas/src/main/java/apirest/personas/serviceimpl/PersonaServiceImpl.java
+++ personas/src/main/java/apirest/personas/serviceimpl/PersonaServiceImpl.java
@@ -9,34 +9,66 @@ import apirest.personas.entities.Persona;
 import apirest.personas.repositories.PersonaRepository;
 import apirest.personas.services.PersonaService;
 
+/**
+ * The PersonaService implementaion class
+ */
 @Service
 public class PersonaServiceImpl implements PersonaService {
-	
-	@Autowired
-	PersonaRepository personaRepo;
 
-	@Override
-	public List<Persona> findAll() {
-		return (List<Persona>) personaRepo.findAll();
-	}
+    /** The PersonaRepository */
+    @Autowired
+    PersonaRepository personaRepo;
 
-	@Override
-	public Persona findById(Long personaId) {
-		return personaRepo.findById(personaId).get();
-	}
+    /**
+     * findAll return all persons
+     * 
+     * @return List<Persona>
+     */
+    @Override
+    public List<Persona> findAll() {
+        return (List<Persona>) personaRepo.findAll();
+    }
+    
+    /**
+     * findById find persona by Id
+     *
+     * @param Long persona Id
+     * @return Persona
+     */
+    @Override
+    public Persona findById(Long personaId) {
+        return personaRepo.findById(personaId).get();
+    }
 
-	@Override
-	public Persona saveNewPersona(Persona persona) {
-		return personaRepo.save(persona);
-	}
+    /**
+     * saveNewPersona create a new person
+     *
+     * @param Persona persona
+     * @return Persona
+     */
+    @Override
+    public Persona saveNewPersona(Persona persona) {
+        return personaRepo.save(persona);
+    }
 
-	@Override
-	public Persona savePersona(Persona persona) {
-		return personaRepo.save(persona);
-	}
-	
-	@Override
-	public void deleteById(Long personaId) {
-		personaRepo.deleteById(personaId);
-	}
+    /**
+     * savePersona update a person
+     *
+     * @param Persona persona
+     * @return Persona
+     */
+    @Override
+    public Persona savePersona(Persona persona) {
+        return personaRepo.save(persona);
+    }
+
+    /**
+     * deleteById delete a person by Id
+     *
+     * @param Long persona Id
+     */
+    @Override
+    public void deleteById(Long personaId) {
+        personaRepo.deleteById(personaId);
+    }
 }
