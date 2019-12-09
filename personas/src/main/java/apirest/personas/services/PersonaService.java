59fd30e79739de35c337d189c24208bf544aed7c
Rodrigo Alconchel <rodalcon@hotmail.com>
2019-12-09 14:20:27 +0100

    javadoc

--- personas/src/main/java/apirest/personas/services/PersonaService.java
+++ personas/src/main/java/apirest/personas/services/PersonaService.java
@@ -4,15 +4,46 @@ import java.util.List;
 
 import apirest.personas.entities.Persona;
 
+/**
+ * The PersonaService Interface
+ */
 public interface PersonaService {
-	
-	public List<Persona> findAll();
-	
-	public Persona findById(Long personaId);
-	
-	public Persona saveNewPersona(Persona persona);
-	
-	public Persona savePersona(Persona persona);
-	
-	public void deleteById(Long personaId);
+
+    /**
+     * findAll return all persons
+     * 
+     * @return List<Persona>
+     */
+    public List<Persona> findAll();
+
+    /**
+     * findById find persona by Id
+     *
+     * @param Long persona Id
+     * @return Persona
+     */
+    public Persona findById(Long personaId);
+
+    /**
+     * saveNewPersona create a new person
+     *
+     * @param Persona persona
+     * @return Persona
+     */
+    public Persona saveNewPersona(Persona persona);
+
+    /**
+     * savePersona update a person
+     *
+     * @param Persona persona
+     * @return Persona
+     */
+    public Persona savePersona(Persona persona);
+
+    /**
+     * deleteById delete a person by Id
+     *
+     * @param Long persona Id
+     */
+    public void deleteById(Long personaId);
 }
