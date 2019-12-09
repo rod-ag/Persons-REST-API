c7ba4d8f8c30629b00da894b59ee13ba468e8bfc
Rodrigo Alconchel <rodalcon@hotmail.com>
2019-12-09 12:47:44 +0100

    javadoc

--- personas/src/main/java/apirest/personas/PersonasApplication.java
+++ personas/src/main/java/apirest/personas/PersonasApplication.java
@@ -3,11 +3,14 @@ package apirest.personas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The PersonasApplication Class
+ */
 @SpringBootApplication
 public class PersonasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonasApplication.class, args);
+    }
 
 }
