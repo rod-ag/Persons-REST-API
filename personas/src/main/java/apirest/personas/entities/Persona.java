c42f8eeff21b850ea3d7825378b072d113e80ef7
Rodrigo Alconchel <rodalcon@hotmail.com>
2019-12-09 13:34:21 +0100

    javadoc

--- personas/src/main/java/apirest/personas/entities/Persona.java
+++ personas/src/main/java/apirest/personas/entities/Persona.java
@@ -11,72 +11,95 @@ import org.springframework.hateoas.RepresentationModel;
 
 import io.swagger.annotations.ApiModelProperty;
 
+/**
+ * The Persona Entity Class
+ */
 @Entity
 @Table(name = "Personas")
 public class Persona extends RepresentationModel<Persona> {
 
-	@ApiModelProperty(value="Id de Persona", required=true, example="1", position=1)
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long personid;
-	
-	@ApiModelProperty(value="Person name", required=true, example="MyName", position=2)
-	@NotBlank(message = "Name cannot be empty")
-	private String name;
-	
-	@ApiModelProperty(value="Person address", required=false, example="21 High Road", position=3)
-	@NotBlank(message = "Address cannot be empty")
-	private String address;
-	
-	@ApiModelProperty(value="Person phone", required=false, example="634339445", position=4)
-	@NotBlank(message = "Phone cannot be empty")
-	private String phone;
+    /** The person's Id */
+    @ApiModelProperty(value = "Id de Persona", required = true, example = "1", position = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long personid;
 
-	Persona() {
-	}
+    /** The person's name */
+    @ApiModelProperty(value = "Person name", required = true, example = "MyName", position = 2)
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
 
-	public Persona(String name, String address, String phone) {
-		this.name = name;
-		this.address = address;
-		this.phone = phone;
-	}
-	
-	public Persona(Long personid, String name, String address, String phone) {
-		this.personid = personid;
-		this.name = name;
-		this.address = address;
-		this.phone = phone;
-	}
+    /** The person's address */
+    @ApiModelProperty(value = "Person address", required = false, example = "21 High Road", position = 3)
+    @NotBlank(message = "Address cannot be empty")
+    private String address;
 
-	public Long getPersonid() {
-		return personid;
-	}
+    /** The  person's phone number */
+    @ApiModelProperty(value = "Person phone", required = false, example = "634339445", position = 4)
+    @NotBlank(message = "Phone cannot be empty")
+    private String phone;
 
-	public void setPersonid(Long personid) {
-		this.personid = personid;
-	}
+    /** Empty constructor */
+    Persona() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    /**
+     * Persona constructor
+     * 
+     * @param name
+     * @param address
+     * @param phone
+     */
+    public Persona(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Persona constructor
+     * 
+     * @param personid
+     * @param name
+     * @param address
+     * @param phone
+     */
+    public Persona(Long personid, String name, String address, String phone) {
+        this.personid = personid;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public Long getPersonid() {
+        return personid;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setPersonid(Long personid) {
+        this.personid = personid;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
